a566d9a9721fd1d06bd046a1a4c6d877b3bcdddf
Jonathan Zarate <zaratedev@gmail.com>
2018-06-22 10:07:21 -0500

    Simple rule with not abbreviations in class and functions.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Not abbreviations of class.
+ */
+class UserRepository
+{
+  // Example #1
+  public function fetch($billingId)
+  {
+    # code...
+  }
+
+  public function fetchByBillingId($id)
+  {
+    # code...
+  }
+}
+
+
+
+
+
+ ?>
